0ca9e14023247b5188028fbb717bbde1a09e7b24
Ibrahim Itani <andrahim@gmail.com>
2016-04-01 16:40:44 -0700

    Client Editing Functionality

--- app/controllers/Clients.scala
+++ app/controllers/Clients.scala
@@ -7,6 +7,8 @@ import play.api.Play.current
 import play.api.data._
 import play.api.data.Forms._
 import java.util.Date
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import play.api.i18n.Messages.Implicits._
 
 import play.api.Logger
@@ -34,7 +36,7 @@ class Clients extends Controller{
   def info(cid: String) = Action {
     var branchList : List[String] = List()
     var data : Any = null
-    val exec = "SELECT name, birthdate, homephonenumber, streetaddr, city " +
+    val exec = "SELECT name, birthdate, homephonenumber, workphonenumber, streetaddr, city, province, postalcode " +
                 "FROM client " +
                 "WHERE client.cid = '" + cid + "'"
     val conn = DB.getConnection()
@@ -44,7 +46,8 @@ class Clients extends Controller{
 
       while (rs.next) {
         if (data == null){
-          data = Client(cid, rs.getString(1), rs.getString(2), rs.getString(3), "workPhonNumPlaceholder", rs.getString(4), rs.getString(5), "province", "postalCode" )
+          data = Client(cid, rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
+            rs.getString(6), rs.getString(7), rs.getString(8) )
         }
       }
     } finally {
@@ -52,15 +55,58 @@ class Clients extends Controller{
     }
     val client : Client = data.asInstanceOf[Client]
     if(client != null){
-      //something to add?
+      val df = new SimpleDateFormat("yyyy-MM-dd");
+      val startDate = df.parse(client.birthDate);
+
+      val clientInfo = ClientForm(client.cid.replaceAll("""(?m)\s+$""", ""),client.name.replaceAll("""(?m)\s+$""", ""),startDate,
+        Integer.parseInt(client.homePhoneNumber.replaceAll("""(?m)\s+$""", "")),
+        Integer.parseInt(client.workPhoneNumber.replaceAll("""(?m)\s+$""", "")),
+        client.streetAddress,
+        client.city.replaceAll("""(?m)\s+$""", ""),
+        client.province.replaceAll("""(?m)\s+$""", ""),
+        client.postalCode.replaceAll("""(?m)\s+$""", ""))
+
+      Ok(views.html.clients.info(client, clientForm.fill(clientInfo)))
     }
-    Ok(views.html.clients.info(client))
+    else {
+      val dummyClient = ClientForm("","",null,0,0,"","","","")
+      Ok(views.html.clients.info(client, clientForm.fill(dummyClient)))
+    }
+
+  }
+
+  def editClient(cid: String) = Action { implicit request =>
+    clientForm.bindFromRequest.fold(
+      formWithErrors => {
+        Logger.debug("in the form with Errors!")
+        BadRequest(views.html.clients.clientCreate(formWithErrors))
+      },
+      client => {
+        Logger.debug("in the success method!")
+        this.edit(client, cid)
+        Redirect(routes.Clients.info(client.cid))
+      }
+    )
+  }
+
+  def edit(form : ClientForm, cid : String): Unit = {
+    val params = "name= " + "'" + form.name + "'" + ", birthdate= " + "'" + form.birthdate + "'" + ", homephonenumber = " + form.homephone.toString +
+    ", workphonenumber = " + form.workphone.toString + ", streetaddr = " + "'" + form.streetAddress + "'" + ", city = " + "'" + form.city + "'" +
+      ", province = " + "'" + form.province + "'" + ", postalcode = " + "'" + form.postalCode + "'"
+    val exec =  "UPDATE client SET " + params + " WHERE cid = " + "'" + form.cid + "'" 
+    Logger.debug(s"Query=$exec")
+    val conn = DB.getConnection()
+    try {
+      val stmt = conn.createStatement;
+      stmt.execute(exec)
+    } finally {
+      conn.close()
+    }
   }
 
   def saveClient = Action { implicit request =>
     clientForm.bindFromRequest.fold(
       formWithErrors => {
-        Logger.debug("inside error")
         BadRequest(views.html.clients.clientCreate(formWithErrors))
       },
       client => {
