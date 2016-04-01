965ae82be94434dd761b88b2ad9ceab8b3a9c782
Jorge Luis Lazo <jorge.lazo@alumni.ubc.ca>
2016-03-31 19:06:47 -0700

    added services class

--- app/models/Service.scala
+++ app/models/Service.scala
@@ -3,6 +3,6 @@ package models
 /**
   * Created by jorgeluis on 31/03/16.
   */
-case class Service(sid : String, serviceTypeName : String, baseFee : Int, hourlyRate : Int, amountPaid : Int) {
+case class Service(sid : String, serviceTypeName : String, baseFee : Int, hourlyRate : Int, amountPaid : Int, TimeEntries : List[Timeentry]) {
 
 }
---
+++ app/models/Timeentry.scala
@@ -0,0 +1,10 @@
+package models
+
+import java.sql.Date
+
+/**
+  * Created by jorgeluis on 31/03/16.
+  */
+case class Timeentry(startDateEntry : Date, endDateTime : Date) {
+
+}
