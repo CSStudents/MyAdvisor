db3830553f4124c9a5e6f75eee9f3a56886bc242
Jorge Luis Lazo <jorge.lazo@alumni.ubc.ca>
2016-04-03 12:46:11 -0700

    Fixed some error handling

--- app/controllers/Offices.scala
+++ app/controllers/Offices.scala
@@ -26,27 +26,41 @@ class Offices extends Controller{
   def info(oid: String) = Action {
     var branchList : List[String] = List()
     var data : Any = null
-    val exec = "SELECT officelocation.streetaddress, city, mainofficenumber, dptname  " +
-                "FROM officelocation, located_at " +
-                "WHERE officelocation.streetaddress = located_at.streetaddress and officelocation.oid = '" + oid + "'"
     val conn = DB.getConnection()
     try {
       val stmt = conn.createStatement
-      val rs = stmt.executeQuery(exec)
-      while (rs.next) {
-        if (data == null){
-          data = Office(rs.getString(1), "PostalCodePlaceHolder", rs.getString(2), "provPlaceHolder" ,rs.getString(3), oid, Nil )
+      val pokeQuery = stmt.executeQuery("select officelocation.streetAddress, officelocation.postalcode, dptname from officelocation, located_at " +
+                        "where officelocation.streetAddress = located_at.streetAddress and officelocation.oid = '" + oid + "'")
+      var hasBranches : Boolean = false
+      while(pokeQuery.next){
+        hasBranches = true
+      }
+      if(hasBranches){ //request all data with branches
+      val exec = "SELECT officelocation.streetaddress, city, mainofficenumber, dptname  " +
+          "FROM officelocation, located_at " +
+          "WHERE officelocation.streetaddress = located_at.streetaddress and officelocation.oid = '" + oid + "'"
+        val rs = stmt.executeQuery(exec)
+        while (rs.next) {
+          if (data == null){
+            data = Office(rs.getString(1), "PostalCodePlaceHolder", rs.getString(2), "provPlaceHolder" ,rs.getString(3), oid, Nil )
+          }
+          branchList = rs.getString(4) :: branchList
         }
-        branchList = rs.getString(4) :: branchList
+      }else{ //no branches found, request only the location info
+      val exec = "SELECT streetaddress, city, mainofficenumber  " +
+          "FROM officelocation WHERE officelocation.oid = '" + oid + "'"
+        val rs = stmt.executeQuery(exec)
+        while (rs.next) {
+          if (data == null){
+            data = Office(rs.getString(1), "PostalCodePlaceHolder", rs.getString(2), "provPlaceHolder" ,rs.getString(3), oid, Nil )
+          }
+        }
       }
     } finally {
       conn.close()
     }
     val office: Office = data.asInstanceOf[Office]
-    if(office != null){
       office.branches = branchList
-    }
     Ok(views.html.offices.info(office))
   }
-
 }
